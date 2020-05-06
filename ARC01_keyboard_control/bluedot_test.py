0ed2fc2dae6092ee8a4ba9a2b4007a9af3fbda0e
root <turegano.jose@gmail.com>
2020-05-06 20:00:33 +0100

    Testing camera

--- ARC01_keyboard_control/bluedot_test.py
+++ ARC01_keyboard_control/bluedot_test.py
@@ -3,24 +3,34 @@ from signal import pause
 import os
 import time
 import numpy as np
-# os.system ("sudo pigpiod") #Launching GPIO library
-# time.sleep(1) # Delay to launch the GPIO library
-# import pigpio 
+os.system ("sudo pigpiod") #Launching GPIO library
+time.sleep(1) # Delay to launch the GPIO library
+import pigpio 
+from picamera import PiCamera
+
+camera = PiCamera()
+camera.rotation = 180
+camera.resolution = (1024, 768)
+
+
 
 ESC = 4 # GPIO 4 (pin 7)
 servo = 17 # GPIO 17 (pin 11)
 bd = BlueDot()
 count = 0.0
 toggle = 1
-speed = 1500.0
+speed = 1645.0
 
-# pi = pigpio.pi();
-# pi.set_servo_pulsewidth(ESC, 0) 
-# pi.set_servo_pulsewidth(servo, 0)
+pi = pigpio.pi();
+pi.set_servo_pulsewidth(ESC, 0) 
+pi.set_servo_pulsewidth(servo, 0)
 
 max_servo_value = 1780 #Servo's max value
 min_servo_value = 980  #Servo's min value
 
+zero_value = (max_servo_value + min_servo_value)/2
+half_range = (max_servo_value - min_servo_value)/2
+    
 max_esc_value = 1780 #ESC's max value
 min_esc_value = 980  #ESC's min value
 
@@ -32,32 +42,35 @@ decrease_throttle_rate = 10
 def move(pos):
     if pos.top:
         update_motor_speed(pos.y*increase_throttle_rate)
-        print('increasing motor speed by',pos.y*increase_throttle_rate)
+        #print('increasing motor speed by',pos.y*increase_throttle_rate)
     elif pos.bottom:
         update_motor_speed(pos.y*decrease_throttle_rate)
-        print('decreasing motor speed by',pos.y*decrease_throttle_rate)
+        #print('decreasing motor speed by',pos.y*decrease_throttle_rate)
 #     elif pos.left:
 #         print('left',pos.distance)
 #     elif pos.right:
 #         print('right',pos.distance)
 
 def stop():
-    print('stop')
     stop_motor()
     stop_servo()
+    #print('stop')
 
 def start_stop():
     global toggle
     global speed
     if toggle == 1:
-        print('starting')
+        #print('starting')
         bd.color = 'green'
         toggle = 0
         motor_speed_start(speed)
+        #camera.start_preview()
     else:
-        print('stoping')
+        #print('stoping')
+        stop()
         bd.color = 'red'
         toggle = 1
+        #camera.stop_preview()
 
 def steering_ipod(angle):
     # Positive right, negative left
@@ -68,7 +81,11 @@ def steering_ipod(angle):
 
 def steering(pos):
     # Positive right, negative left
-    print('angle',pos.angle)
+    #print('angle',pos.angle)
+    global count
+    count += 1.0
+    print(count)
+    steering_servo(pos.angle)
     
 ## ESC and servo functions
 def update_motor_speed(speed_update):
@@ -76,28 +93,29 @@ def update_motor_speed(speed_update):
     speed += speed_update
     speed = np.clip(speed, min_esc_value, max_esc_value)
     #pi.set_servo_pulsewidth(ESC, speed)
-    print('increasing speed',speed)
+    #print('increasing speed',speed)
     
 def motor_speed_start(speed):
     speed = np.clip(speed, min_esc_value, max_esc_value)
-    #pi.set_servo_pulsewidth(ESC, speed)
-    print('speed',speed)
+    pi.set_servo_pulsewidth(ESC, speed)
+    #print('speed',speed)
     
 def stop_motor(): #stop ESC motor
-    #pi.set_servo_pulsewidth(ESC, 0)
-    print('stopping motor')
+    pi.set_servo_pulsewidth(ESC, 0)
+    #print('stopping motor')
     
 def stop_servo(): #stop servo
-    #pi.set_servo_pulsewidth(servo, 0)
-    print('stopping servo')
+    pi.set_servo_pulsewidth(servo, 0)
+    #print('stopping servo')
 
 def steering_servo(angle): # steering servo
-    zero_value = (max_servo_value + min_servo_value)/2
-    half_range = (max_servo_value - min_servo_value)/2
-    steering =  zero_value + half_range * angle/180 # angle goes from -180 to 180
+    steering =  zero_value - half_range * angle/180 # angle goes from -180 to 180
     steering = np.clip(steering, min_servo_value, max_servo_value)
-    #pi.set_servo_pulsewidth(servo, steering)
+    pi.set_servo_pulsewidth(servo, steering)
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    #camera.capture('/media/pi/UUI/'+timestr+'str'+str(steering)+'.jpg')
     #print('steering', steering)
+    print('/media/pi/UUI/'+timestr+'str'+str(steering)+'.jpg')
 
 bd.rotation_segments = 180
 bd.color = 'red'
@@ -109,5 +127,5 @@ bd.color = 'red'
 bd.when_double_pressed = start_stop
 #bd.when_rotated = steering_ipod
 bd.when_moved = steering
-
+print('hola')
 pause()
--- ARC01_keyboard_control/camera_test.py
+++ ARC01_keyboard_control/camera_test.py
@@ -1,8 +1,58 @@
-from picamera import PiCamera
-from time import sleep
+import picamera
+import time
+import io
+from picamera.array import PiRGBArray
 
-camera = PiCamera()
-camera.rotation = 180
-camera.start_preview()
-sleep(55)
-camera.stop_preview()
+with picamera.PiCamera() as camera:
+#     camera.rotation = 180
+#     camera.start_preview()
+#     time.sleep(5)
+#     camera.stop_preview()
+#     camera.resolution = (1024, 768)
+#     camera.start_preview()
+#     # Camera warm-up time
+#     time.sleep(2)
+#     timestr = time.strftime("%Y%m%d-%H%M%S")
+#     camera.capture('/media/pi/UUI/'+timestr+'.jpg')
+#     print(timestr)
+    
+    camera.resolution = (1024, 768)
+#     camera.start_recording('/media/pi/UUI/my_video.h264')
+#     camera.wait_recording(60)
+#     camera.stop_recording()
+#     timestr = time.strftime("%Y%m%d-%H%M%S")
+#     print(timestr)
+
+
+
+    ## stream
+#     camera.framerate = 80
+#     time.sleep(1)
+#     start = time.time()
+#     # Set up 40 in-memory streams
+#     for x in range(10):
+#         outputs = io.BytesIO(40)
+#         timestr = time.strftime("%Y%m%d-%H%M%S")
+#         camera.capture(outputs, format='bgr', use_video_port=True)
+#     finish = time.time()
+#         # How fast were we?
+#     print('Captured 1 image at %.2fs' % ( (finish - start))) 
+
+
+    start = time.time()
+    rawCapture = PiRGBArray(camera, size=(1024, 768))
+    camera.framerate = 80
+    count=0
+    # capture frames from the camera
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        image = frame.array
+
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+        count +=1
+        # if the `q` key was pressed, break from the loop
+        if count == 10:
+            break
+    finish = time.time()
+    print(count)
+    print('Captured x image at %.2fs' % ( (finish - start))) 
